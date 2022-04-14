5ac8904b6fdde1d2554cbfd826c2acf8bf32f084
Davi Serrano <daviscardoso2901@gmail.com>
2022-04-13 22:22:33 -0300

    Refatorandi Specifications

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,17 +1,10 @@
 import { Router } from "express" 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationRoutes = Router();
-const specificationsRepository = new SpecificationsRepository()
 
 specificationRoutes.post("/", (request, response)=>{
-    const { name, description} = request.body;
-    const createSpecificationService = new CreateSpecificationService(specificationsRepository);
-    
-    createSpecificationService.execute({name, description});
+    return createSpecificationController.handle(request, response)
+});
 
-    return response.status(201).send()
-})
-
-export { specificationRoutes }
+export { specificationRoutes } ; 
