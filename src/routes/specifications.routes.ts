5e470d0dc84a718b810b9b1e6e5759f17dec4ea0
Davi Serrano <daviscardoso2901@gmail.com>
2022-05-07 21:07:09 -0300

    specifications routes pro plural

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express" 
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
-const specificationRoutes = Router();
+const specificationsRoutes = Router();
 
 specificationRoutes.post("/", (request, response)=>{
     return createSpecificationController.handle(request, response)
 });
 
-export { specificationRoutes } ; 
+export { specificationsRoutes } ; 
