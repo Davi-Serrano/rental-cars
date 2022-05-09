28dc367caf20f7503c75e711bc1d572b1ac48f35
Davi Serrano <daviscardoso2901@gmail.com>
2022-05-09 20:07:01 -0300

    Configurando TSyringe

--- src/modules/cars/useCases/createCategory/createCategoryController.ts
+++ src/modules/cars/useCases/createCategory/createCategoryController.ts
@@ -1,17 +1,20 @@
 import { Request, Response } from "express"
+
+import { container } from "tsyringe";
+
 import { CreateCategoryUseCase } from "./CreateCategoryuseCase";
 
 
 class CreateCategoryController {
-    constructor(private createCategoriesUseCase: CreateCategoryUseCase){}
-
     async handle(request: Request, response: Response): Promise<Response>{
-    const { name, description } = request.body;
+        const { name, description } = request.body;
 
-    await this.createCategoriesUseCase.execute({name, description});
+        const createCategoriesUseCase = container.resolve(CreateCategoryUseCase) 
 
-    return response.status(201).send();
+        await createCategoriesUseCase.execute({name, description});
+
+        return response.status(201).send();
     }
 }
 
-export {  CreateCategoryController}
+export { CreateCategoryController }
