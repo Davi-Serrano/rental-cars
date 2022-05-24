f98fb1fc904fe9e74a06be97bca33a79ababcd01
Davi Serrano <daviscardoso2901@gmail.com>
2022-05-23 22:12:18 -0300

    Criandondo repositorio de usuario

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,23 @@
+import { inject } from "tsyringe";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUserRepository } from "../../reposiroies/IUserRepository";
+
+
+class CreateUserUseCase {
+    constructor(
+        @inject("UserRepository")
+        private userRepository: IUserRepository
+    ){}
+
+    async execute({name, username, driver_license, email, password}: ICreateUserDTO): Promise<void>{
+        await this.userRepository.create({
+            name, 
+            username, 
+            driver_license, 
+            email, 
+            password
+        });
+    }
+}
+
+export { CreateUserUseCase };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,14 @@
 import  {container}  from "tsyringe"
+
+import { UserRepository } from "../../modules/accounts/reposiroies/implementations/UserRepository";
+import { IUserRepository } from "../../modules/accounts/reposiroies/IUserRepository";
+
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository"
-import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository"
+
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -12,4 +18,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
     "SpecificationsRepository",
     SpecificationsRepository
+);
+
+container.registerSingleton<IUserRepository>(
+    "UserRepository",
+    UserRepository
 );
